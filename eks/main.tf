3b1d45df527c87bfb9cb1df025e872c7835a1cf6
Shrey Kejriwal <shreykejriwal@gmail.com>
2024-09-18 01:09:26 +0530

    Enable cluster public access

--- eks/main.tf
+++ eks/main.tf
@@ -40,6 +40,8 @@ module "eks" {
   cluster_name    = "pdf-hero-eks-cluster"
   cluster_version = "1.30"
 
+  cluster_endpoint_public_access  = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
