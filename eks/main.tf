daa7d03529819e27cd6b4acfb4c607f141eee861
Shrey Kejriwal <shreykejriwal@gmail.com>
2024-09-18 22:13:01 +0530

    Add Cluster access to current user

--- eks/main.tf
+++ eks/main.tf
@@ -37,6 +37,10 @@ module "eks" {
 
   cluster_endpoint_public_access  = true
 
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
